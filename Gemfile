68043172d40336af3cf262ca3592181b0049ce88
namdongngac <ntnam7399@gmail.com>
2019-12-12 10:32:59 +0700

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'faker', '1.7.3'
 # Pagination
 gem 'will_paginate', '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+# Upload Image
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
@@ -70,6 +73,7 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,17 @@ User.create!(
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
-User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password)
+User.create!(
+    name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(5)
+50.times do
+    content = Faker::Lorem.sentence(5)
+    users.each {|user| user.microposts.create!(content: content)}
 end
