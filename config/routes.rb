eff0a7193e47e10eff911fef7d4e6cd8c1f2d0c0
namdongngac <ntnam7399@gmail.com>
2019-12-17 10:18:26 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'users/new'
   root 'static_pages#home'
+
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
@@ -14,9 +15,14 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logout', to: 'static_pages#home'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:edit, :new, :create, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,9 +5,10 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example", 
-                    email: "user@example.com", 
-                    password: "foobar", password_confirmation:"foobar")
+    @user = User.new(
+      name: "Example", 
+      email: "user@example.com", 
+      password: "foobar", password_confirmation:"foobar")
   end
   test "should be valid" do
     assert @user.valid?
@@ -74,6 +75,35 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+    end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # post from following
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # post from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # post from unfollowed
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)   
     end
   end
 end
